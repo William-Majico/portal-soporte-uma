2d4d98684e14c6fea3cd0e45a0d7e7074aad5e3c
Enrique3D <majicoenrique76@gmail.com>
2026-03-24 17:17:51 -0600

    Configuración de despliegue para GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  // 1. Pon tu URL de GitHub Pages (Cámbialo por tu usuario real)
+  site: 'https://William-Majico.github.io', 
+  
+  // 2. IMPORTANTE: Pon el nombre exacto de tu repositorio aquí
+  base: '/portal-soporte-uma', 
+
   vite: {
     plugins: [tailwindcss()]
   }
