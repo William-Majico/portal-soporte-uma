a47621204688d644f8bf05fe9f4745b72a2a7e18
Enrique3D <majicoenrique76@gmail.com>
2026-03-24 17:58:27 -0600

    Corrección de rutas para despliegue

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,9 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
-  // 1. Pon tu URL de GitHub Pages (Cámbialo por tu usuario real)
-  site: 'https://William-Majico.github.io', 
-  
-  // 2. IMPORTANTE: Pon el nombre exacto de tu repositorio aquí
-  base: '/portal-soporte-uma', 
-
+  site: 'https://william-majico.github.io',
+  base: '/portal-soporte-uma',
   vite: {
     plugins: [tailwindcss()]
   }
